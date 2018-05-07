6105bdbfdae922224659a38f9ccf4ebe480e592e
Kimberly Swope <Kim.swope@yahoo.com>
2018-05-07 12:13:11 -0400

    first major commit minus public files

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
+var routes = require('./controllers/routes.js');
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(methodOverride('_method'));
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+app.use('/', routes);
+// Log (server-side) when our server has started
+
+app.listen (function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
